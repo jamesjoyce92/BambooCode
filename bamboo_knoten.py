1654cd4f6d77a60133352e7ccb2ba6ba1ddcb83a
Majd <77399628+Majd949@users.noreply.github.com>
2021-01-22 23:06:53 +0100

    improved loop
    bug while saving values in dictionary... line 135 sol_dic

--- bamboo_knoten.py
+++ bamboo_knoten.py
@@ -32,7 +32,6 @@ class bamboo:
             for j in range(len(self.kPos)):
                 if j < len(self.kPos) - 1 - i:
                     temp.append(abs(self.kPos[i] - self.kPos[j + 1 + i]))
-
             if temp:
                 messure_dic["dist{}{}".format(self.id, i)] = distances(self.id, temp)
 
@@ -42,7 +41,6 @@ class distances:
     def __init__(self, id, dist):
         self.id = id
         self.dist = dist
-        #hello
 
     def messure_info(self):
         return self.id, self.dist
@@ -90,10 +88,9 @@ def get_longest_list(lst):
 # print(find_by_index(0,0,"ALL"))
 # print(find_by_index(0,0,2))
 
+# desired_bamboo = [370, 130, 240, 100, 190, 210, 390]
+
 pos_desired_bamboo = pd.read_csv("pos_desired_bamboo.csv")
-
-# desired_bamboo = [370, 130, 240, 100, 190, 210, 390, 310, 370, 590, 256, 222, 435, 480]
-
 
 des_values_comb = pos_desired_bamboo.values.tolist()
 
@@ -111,6 +108,7 @@ bamboo_pos_len = 5
 
 add_bamboos()
 
+
 while first_ind < bamboo_num and repeat == True:
     second_ind = 0
     while second_ind < bamboo_pos_len:
@@ -120,7 +118,7 @@ while first_ind < bamboo_num and repeat == True:
             if des_values[item] in ac[1]:
                 val_ind = ac[1].index(des_values[item])
                 temp_sol.append((first_ind, second_ind, des_values[item]))
-                print(temp_sol[-1])
+                # print(temp_sol[-1])
                 des_values.pop(item)
                 match += 1
                 if val_ind > 0:
@@ -129,14 +127,14 @@ while first_ind < bamboo_num and repeat == True:
                 break
             item += 1
         second_ind += 1
-
     first_ind += 1
     if first_ind == bamboo_num:
+        print(s, temp_sol)
         sol_dic["sol_{}".format(s)] = temp_sol
         s += 1
-        if match != len(des_values) and s != 1000:
+        if match != len(des_values) and s < len(des_values_comb):
             fail += 1
-            print("\nFailure")
+            # print("\nFailure")
             add_bamboos()
             first_ind = 0
             des_values = des_values_comb[s]
@@ -145,8 +143,18 @@ while first_ind < bamboo_num and repeat == True:
         else:
             print("success")
 
-# print(sol_dic["sol_0"])
-print("{}/{} desired lengths were found.".format(match, len(des_values)))
+# # print(sol_dic["sol_0"])
+# print("{}/{} desired lengths were found.".format(match, len(des_values)))
 
-print(get_longest_list(list(sol_dic.values()))[0])
-print(get_longest_list(list(sol_dic.values()))[1])
+clean_sol_dic = {}
+
+for key, value in sol_dic.items():
+    if value not in clean_sol_dic.values():
+        clean_sol_dic[key] = value
+
+print(get_longest_list(list(clean_sol_dic.values()))[0])
+print(get_longest_list(list(clean_sol_dic.values()))[1])
+
+# print(sol_dic)
+
+# print(sol_dic["sol_999"])
